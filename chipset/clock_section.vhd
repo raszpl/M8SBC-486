40d6bbc8a7bc445d0be8b9276281b576a931511c
Rasz_pl <citizenr@gmail.com>
2026-02-03 02:22:15 +0100

    tabs, formatting, extension

--- chipset/clock_section.vhd
+++ chipset/clock_section.vhd
@@ -17,9 +17,6 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
-
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
