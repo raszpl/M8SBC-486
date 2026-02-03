31db4903f9c69348798e84338e7dcb3754576a0f
Rasz_pl <citizenr@gmail.com>
2026-02-03 03:45:45 +0100

    tabs, formatting, case

--- chipset/clock_section_pit.vhd
+++ chipset/clock_section_pit.vhd
@@ -22,7 +22,6 @@ USE IEEE.STD_LOGIC_1164.ALL;
 LIBRARY UNISIM;
 USE UNISIM.VCOMPONENTS.ALL;
 
-
 ENTITY clock_section_pit IS
 	PORT (
 		CLK_INPUT	: IN  STD_LOGIc;  -- 14.318 MHz in
@@ -38,34 +37,34 @@ ARCHITECTURE Behavioral OF clock_section_pit IS
 BEGIN
 
 	CLKDLL_inst : CLKDLL
-	generic map (
+	GENERIC MAP (
 		CLKDV_DIVIDE => 12.0, -- "invalid" but works?!
 		  -- We can divide by: 1.5,2.0,2.5,3.0,4.0,5.0,8.0 or 16.0
 		DUTY_CYCLE_CORRECTION => TRUE,
 		STARTUP_WAIT => TRUE
 	)
-	port map (
-		CLKIN	=> CLK_INPUT, -- from input buffer (AT MAIN)
-		CLKFB	=> CLKFB,	  -- feedback from BUFG
-		RST		=> '0',		  -- no reset
-		CLKDV	=> CLKDV_raw, -- divided clock
-		CLK0	=> CLK0_raw,  -- 0° output for feedback
-		CLK90	=> open,
-		CLK180	=> open,
-		CLK270	=> open,
-		CLK2X	=> open,
+	PORT MAP (
+		CLKIN	=> CLK_INPUT,	-- from input buffer (AT MAIN)
+		CLKFB	=> CLKFB,		-- feedback from BUFG
+		RST		=> '0',			-- no reset
+		CLKDV	=> CLKDV_raw,	-- divided clock
+		CLK0	=> CLK0_raw,	-- 0Â° output for feedback
+		CLK90	=> OPEN,
+		CLK180	=> OPEN,
+		CLK270	=> OPEN,
+		CLK2X	=> OPEN,
 		LOCKED	=> LOCKED_int
 	);
 
 
 	BUFG_FB : BUFG
-	port map (
+	PORT MAP (
 		I => CLK0_raw,
 		O => CLKFB
 	);
 
 	BUFG_CLKDV : BUFG
-	port map (
+	PORT MAP (
 		I => CLKDV_raw,
 		O => CLK_OUT
 	);
